e5feac7a829b04d601578179281c9ac4ff6f18ae
Thompson <messagethompson@gmail.com>
2018-05-15 15:55:01 -0700

    working on stuff

--- Pages/navbar.js
+++ Pages/navbar.js
@@ -1,4 +1,4 @@
-function CreateNavbar(){
+function createNavbar(){
     
     // get navigation div reference
     
@@ -8,6 +8,7 @@ function CreateNavbar(){
     
     let nav = document.createElement("nav");
     nav.classList.add('navbar','navbar-expand-lg','navbar-dark');
+    //nav.setAttribute('style','background-color: #3f7547');
     navDiv.appendChild(nav);
     
     // current page
@@ -53,11 +54,13 @@ function CreateNavbar(){
     });
     nav.appendChild(collapseDiv);
     
+    // collapse list
+    
     let collapseUl = createElement('ul');
     collapseUl.classList.add('navbar-nav','mr-auto');
     collapseDiv.appendChild(collapseUl);
     
-    (i = 0; i < 3; i++){
+    for (i = 0; i < 3; i++){
         var li = createElement('li');
         li.classList.add('nav-item');
         collapseUl.appendChild(li);
@@ -76,13 +79,48 @@ function CreateNavbar(){
     anchorTwo.innerHTML = 'Community Tips';
     
     collapseUl.childNodes[2].classList.add('dropdown');
-    let achorThree = collapseUl.childNodes[2].childNodes[0];
+    let anchorThree = collapseUl.childNodes[2].childNodes[0];
+    anchorThree.classList.add('dropdown-toggle');
+    setAttributes(anchorThree,{
+        'href':'#',
+        'id':'navbarDropdown',
+        'role':'button',
+        'data-toggle':'dropdown',
+        'aria-haspopup':'true',
+        'aria-expanded':'false'
+    });
+    anchorThree.innerHTML = "Get Involved";
     
+    // drop down options
     
+    let dropdownDiv = createElement('div');
+    dropdownDiv.classList.add('dropdown-menu');
+    dropdownDiv.setAttribute('aria-labelledby','navbarDropdown');
+    collapseUl.childNodes[2].appendChild(dropdownDiv);
     
+    for (i = 0;i < 5;i++){
+        var a = createElement('a');
+        a.classList.add('dropdown-item');
+        dropdownDiv.appendChild(a);
+        i++
+        if(i < 4){
+            var div = createElement('div');
+            div.classList.add('dropdown-divider');
+            dropdownDiv.appendChild(div);
+        }
+    }
+    
+    dropdownDiv.childNodes[0].setAttribute('href','http://www.lovefoodhatewaste.ca');
+    dropdownDiv.childNodes[0].innerHTML = "Love Food Hate Waste";
+    
+    dropdownDiv.childNodes[2].setAttribute('href','https://www.foodbank.bc.ca/our-programs/food-recovery/');
+    dropdownDiv.childNodes[2].innerHTML = "Greater Vancouver Foodban";
+    
+    dropdownDiv.childNodes[4].setAttribute('href','#');
+    dropdownDiv.childNodes[4].innerHTML = "Link to Another team's project";
 }
 
-function createElement (string input){
+function createElement (input){
     return document.createElement(input);
 }
 
